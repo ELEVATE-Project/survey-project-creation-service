f40c490e375ad97e414d6a9d5224e77458aadb6e
adithya_dinesh <adithya@shikshalokam.org>
2024-05-28 15:22:13 +0530

    review comments

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,3 +1,6 @@
 module.exports = {
 	USERS_LIST: process.env.USER_LIST_ENDPOINT ? process.env.USER_LIST_ENDPOINT : 'v1/account/search',
+	ORGANIZATION_READ: process.env.ORGANIZATION_READ_ENDPOINT
+		? process.env.ORGANIZATION_READ_ENDPOINT
+		: 'v1/organization/read',
 }
--- src/requests/user.js
+++ src/requests/user.js
@@ -24,14 +24,9 @@ const fetchDefaultOrgDetails = function (organisationIdentifier) {
 		try {
 			let orgReadUrl
 			if (!isNaN(organisationIdentifier)) {
-				orgReadUrl =
-					userBaseUrl + process.env.ORGANIZATION_READ_ENDPOINT + '?organisation_id=' + organisationIdentifier
+				orgReadUrl = userBaseUrl + endpoints.ORGANIZATION_READ + '?organisation_id=' + organisationIdentifier
 			} else {
-				orgReadUrl =
-					userBaseUrl +
-					process.env.ORGANIZATION_READ_ENDPOINT +
-					'?organisation_code=' +
-					organisationIdentifier
+				orgReadUrl = userBaseUrl + endpoints.ORGANIZATION_READ + '?organisation_code=' + organisationIdentifier
 			}
 
 			let internalToken = true
