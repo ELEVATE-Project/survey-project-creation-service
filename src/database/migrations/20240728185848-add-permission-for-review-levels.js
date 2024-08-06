b4e792b2866d59a136890e579b621a6b056698dd
Priyanka <priyanka@tunerlabs.com>
2024-08-06 14:33:07 +0530

    fix

--- src/database/migrations/20240728185848-add-permission-for-review-levels.js
+++ src/database/migrations/20240728185848-add-permission-for-review-levels.js
@@ -22,54 +22,58 @@ const getPermissionId = async (module, request_type, api_path) => {
 
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		await queryInterface.bulkDelete('modules', null, {})
+		try {
+			//create module
+			const modulesData = [
+				{ code: 'review-stages', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
+			]
 
-		//create module
-		const modulesData = [
-			{ code: 'review-stages', status: 'ACTIVE', created_at: new Date(), updated_at: new Date() },
-		]
+			// Insert the data into the 'modules' table
+			await queryInterface.bulkInsert('modules', modulesData)
 
-		// Insert the data into the 'modules' table
-		await queryInterface.bulkInsert('modules', modulesData)
+			//create permission
+			const permissionsData = [
+				{
+					code: 'review_stages_permissions',
+					module: 'review-stages',
+					request_type: ['PUT', 'GET'],
+					api_path: '/scp/v1/review-stages/update*',
+					status: 'ACTIVE',
+					created_at: new Date(),
+					updated_at: new Date(),
+				},
+			]
 
-		//create permission
-		const permissionsData = [
-			{
-				code: 'review_stages_permissions',
-				module: 'review-stages',
-				request_type: ['PUT', 'GET'],
-				api_path: '/scp/v1/review-stages/update*',
-				status: 'ACTIVE',
-				created_at: new Date(),
-				updated_at: new Date(),
-			},
-		]
+			await queryInterface.bulkInsert('permissions', permissionsData)
 
-		await queryInterface.bulkInsert('permissions', permissionsData)
+			//create role permission mapping
+			const rolePermissionsData = [
+				{
+					role_title: common.ADMIN_ROLE,
+					permission_id: await getPermissionId('review-stages', ['PUT', 'GET'], '/scp/v1/review-stages/*'),
+					module: 'review-stages',
+					request_type: ['PUT', 'GET'],
+					api_path: '/scp/v1/review-stages/*',
+					created_at: new Date(),
+					updated_at: new Date(),
+					created_by: 0,
+				},
+				{
+					role_title: common.ORG_ADMIN_ROLE,
+					permission_id: await getPermissionId('review-stages', ['PUT', 'GET'], '/scp/v1/review-stages/*'),
+					module: 'review-stages',
+					request_type: ['PUT', 'GET'],
+					api_path: '/scp/v1/review-stages/*',
+					created_at: new Date(),
+					updated_at: new Date(),
+					created_by: 0,
+				},
+			]
 
-		//create role permission mapping
-		const rolePermissionsData = [
-			{
-				role_title: common.ADMIN_ROLE,
-				permission_id: await getPermissionId('review-stages', ['PUT', 'GET'], '/scp/v1/review-stages/*'),
-				module: 'review-stages',
-				request_type: ['PUT', 'GET'],
-				api_path: '/scp/v1/review-stages/*',
-				created_at: new Date(),
-				updated_at: new Date(),
-				created_by: 0,
-			},
-			{
-				role_title: common.ORG_ADMIN_ROLE,
-				permission_id: await getPermissionId('review-stages', ['PUT', 'GET'], '/scp/v1/review-stages/*'),
-				module: 'review-stages',
-				request_type: ['PUT', 'GET'],
-				api_path: '/scp/v1/review-stages/*',
-				created_at: new Date(),
-				updated_at: new Date(),
-				created_by: 0,
-			},
-		]
+			await queryInterface.bulkInsert('role_permission_mapping', rolePermissionsData)
+		} catch (error) {
+			console.error(error)
+		}
 	},
 
 	down: async (queryInterface, Sequelize) => {},
