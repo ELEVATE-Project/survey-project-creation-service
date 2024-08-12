6546973048711811828bb40376875ba0de43e91b
adithya_dinesh <adithya@shikshalokam.org>
2024-08-12 13:34:00 +0530

    comments validator added

--- src/validators/v1/reviews.js
+++ src/validators/v1/reviews.js
@@ -13,6 +13,24 @@ module.exports = {
 			.withMessage('id param is empty, please append a valid resource id')
 			.isNumeric()
 			.withMessage('id param is invalid, must be an integer')
+		req.checkBody('comment')
+			.optional()
+			.isArray()
+			.withMessage('Comment must be an array if provided')
+			.custom((comments) => {
+				if (Array.isArray(comments)) {
+					return comments.every(
+						(commentObj) =>
+							typeof commentObj === 'object' &&
+							commentObj !== null &&
+							commentObj.hasOwnProperty('comment') &&
+							commentObj.hasOwnProperty('context') &&
+							commentObj.hasOwnProperty('page')
+					)
+				} else {
+					throw new Error('Comment must be an array of objects with "comment", "context", and "page" keys')
+				}
+			})
 	},
 	approve: (req) => {
 		req.checkParams('id')
@@ -29,5 +47,23 @@ module.exports = {
 			.withMessage('id param is empty, please append a valid resource id')
 			.isNumeric()
 			.withMessage('id param is invalid, must be an integer')
+		req.checkBody('comment')
+			.optional()
+			.isArray()
+			.withMessage('Comment must be an array if provided')
+			.custom((comments) => {
+				if (Array.isArray(comments)) {
+					return comments.every(
+						(commentObj) =>
+							typeof commentObj === 'object' &&
+							commentObj !== null &&
+							commentObj.hasOwnProperty('comment') &&
+							commentObj.hasOwnProperty('context') &&
+							commentObj.hasOwnProperty('page')
+					)
+				} else {
+					throw new Error('Comment must be an array of objects with "comment", "context", and "page" keys')
+				}
+			})
 	},
 }
