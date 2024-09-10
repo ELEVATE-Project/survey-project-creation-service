878b1600572e03bef1c9a86f3044c2a4df2b95d9
Priyanka <priyanka@tunerlabs.com>
2024-09-10 17:05:32 +0530

    permission list

--- src/integration-tests/commonTests.js
+++ src/integration-tests/commonTests.js
@@ -18,25 +18,22 @@ const logIn = async () => {
 			interval: 2500, // poll interval in ms, default 250ms
 			timeout: 100000,
 		}
-		console.log('============>LOGIN 2 : ')
 		await waitOn(opts)
-		console.log('============>LOGIN 3 : ')
 		jest.setTimeout(10000)
-		console.log('============>LOGIN 4 : ')
 		let email = 'adithya.d' + crypto.randomBytes(5).toString('hex') + '@pacewisdom.com'
-		let password = 'WWWWWelcome@@@123'
+		let password = 'Welcome@123'
 		let res = await request.post('/user/v1/account/create').send({
 			name: 'adithya',
 			email: email,
 			password: password,
 		})
-		console.log('============>LOGIN 5 : ')
+		console.log('============>LOGIN 5 : ', res.body, res.statusCode)
 		res = await request.post('/user/v1/account/login').send({
 			email: email,
 			password: password,
 		})
 		console.log('============>LOGIN 6 : ')
-		if (res.body.result.access_token && res.body.result.user.id) {
+		if (res.body && res.body.result && res.body.result.access_token && res.body.result.user.id) {
 			defaultHeaders = {
 				'X-auth-token': 'bearer ' + res.body.result.access_token,
 				Connection: 'keep-alive',
@@ -47,11 +44,12 @@ const logIn = async () => {
 			global.userId = res.body.result.user.id
 			return {
 				token: res.body.result.access_token,
-				refreshToken: res.body.result.refresh_token,
-				userId: res.body.result.user.id,
+				id: res.body.result.user.id,
 				email: email,
 				password: password,
 				name: res.body.result.user.name,
+				roles: res.body.result.user.user_roles,
+				organization_id: res.body.result.user.organization_id,
 			}
 		} else {
 			console.error('Error while getting access token')
@@ -61,6 +59,7 @@ const logIn = async () => {
 		console.error('ERROR : : :', error)
 	}
 }
+
 function logError(res) {
 	let successCodes = [200, 201, 202]
 	if (!successCodes.includes(res.statusCode)) {
