38fec5c9eea82933d296a1e75696c615b16f0758
Priyanka <priyanka@tunerlabs.com>
2024-09-12 10:10:16 +0530

    form completion

--- src/integration-tests/form/form.spec.js
+++ src/integration-tests/form/form.spec.js
@@ -8,75 +8,62 @@ describe('Form APIs', function () {
 	beforeAll(async () => {
 		await commonHelper.logIn()
 	})
-	it('/create', async () => {
-		let res = await request.post('/scp/v1/form/create').send({
-			type: faker.random.alpha(5),
-			sub_type: faker.random.alpha(5),
-			action: faker.random.alpha(5),
-			data: {
-				template_name: 'defaultTemplate',
-				fields: {
-					controls: [
-						{
-							name: 'categories',
-							label: 'Select categories',
-							value: '',
-							class: 'ion-margin',
-							type: 'chip',
-							position: '',
-							disabled: false,
-							showSelectAll: true,
-							validators: {
-								required: true,
-							},
-						},
-					],
-				},
-			},
-		})
-		console.log(JSON.stringify(res.body, null, 2))
+
+	it('Create Form', async () => {
+		let res = await request.post('/scp/v1/form/create').send(insertFormData())
+
 		expect(res.statusCode).toBe(201)
 		expect(res.body).toMatchSchema(schema.createSchema)
 
 
 	})
 
-	it('/read', async () => {
-		let res = await request.get('/scp/v1/form/read/' + formId)
+	it('Read form', async () => {
+		let res = await request.get('/scp/v1/form/read/')
 
-		//console.log(res.body)
 		expect(res.statusCode).toBe(200)
 		expect(res.body).toMatchSchema(schema.readSchema)
 	})
-	// it('/update', async () => {
-	// 	let entityId = await formData.insertForm()
-	// 	let res = await request.post('/mentoring/v1/form/update/' + entityId).send({
-	// 		type: faker.random.alpha(5),
-	// 		subType: faker.random.alpha(5),
-	// 		action: faker.random.alpha(5),
-	// 		data: {
-	// 			templateName: 'defaultTemplate',
-	// 			fields: {
-	// 				controls: [
-	// 					{
-	// 						name: 'categories',
-	// 						label: 'Select categories',
-	// 						value: '',
-	// 						class: 'ion-margin',
-	// 						type: 'chip',
-	// 						position: '',
-	// 						disabled: false,
-	// 						showSelectAll: true,
-	// 						validators: {
-	// 							required: true,
-	// 						},
-	// 					},
-	// 				],
-	// 			},
-	// 		},
-	// 	})
-	// 	//console.log(res.body)
-	// 	expect(res.statusCode).toBe(202)
-	// 	expect(res.body).toMatchSchema(schema.updateSchema)
-	// })
+
+	it('/update', async () => {
+		let formData = await request.post('/scp/v1/form/create').send(insertFormData())
+		if (formData.meta && formData.meta.formVersion && formData.meta.formVersion.length > 0) {
+			let formId = formData.meta.formVersion[0].id
+			let res = await request.post('/scp/v1/form/update/' + formId).send(insertFormData())
+
+			expect(res.statusCode).toBe(200)
+			expect(res.body).toMatchSchema(schema.updateSchema)
+		}
+
+	})
 })
+
+function insertFormData() {
+	let formData = {
+		type: faker.random.alpha(5),
+		sub_type: faker.random.alpha(5),
+		action: faker.random.alpha(5),
+		data: {
+			template_name: 'defaultTemplate',
+			fields: {
+				controls: [
+					{
+						name: 'categories',
+						label: 'Select categories',
+						value: '',
+						class: 'ion-margin',
+						type: 'chip',
+						position: '',
+						disabled: false,
+						showSelectAll: true,
+						validators: {
+							required: true,
+						},
+					},
+				],
+			},
+		},
+	}
+
+	return formData
+}
--- src/integration-tests/form/responseSchema.js
+++ src/integration-tests/form/responseSchema.js
@@ -29,122 +29,80 @@ const createSchema = {
 	required: ['responseCode', 'message', 'result', 'meta'],
 }
 const readSchema = {
-	type: 'object',
-	properties: {
-		responseCode: {
-			type: 'string',
+	'type': 'object',
+	'properties': {
+		'responseCode': {
+			'type': 'string'
 		},
-		message: {
-			type: 'string',
+		'message': {
+			'type': 'string'
 		},
-		result: {
-			type: 'object',
-			properties: {
-				data: {
-					type: 'object',
-					properties: {
-						templateName: {
-							type: 'string',
+		'result': {
+			'type': 'array',
+			'items': [
+				{
+					'type': 'object',
+					'properties': {
+						'id': {
+							'type': 'integer'
 						},
-						fields: {
-							type: 'object',
-							properties: {
-								controls: {
-									type: 'array',
-									items: [
-										{
-											type: 'object',
-											properties: {
-												name: {
-													type: 'string',
-												},
-												label: {
-													type: 'string',
-												},
-												value: {
-													type: 'string',
-												},
-												class: {
-													type: 'string',
-												},
-												type: {
-													type: 'string',
-												},
-												position: {
-													type: 'string',
-												},
-												disabled: {
-													type: 'boolean',
-												},
-												showSelectAll: {
-													type: 'boolean',
-												},
-												validators: {
-													type: 'object',
-													properties: {
-														required: {
-															type: 'boolean',
-														},
-													},
-													required: ['required'],
-												},
-											},
-											required: [
-												'name',
-												'label',
-												'value',
-												'class',
-												'type',
-												'position',
-												'disabled',
-												'showSelectAll',
-												'validators',
-											],
-										},
-									],
-								},
-							},
-							required: ['controls'],
+						'type': {
+							'type': 'string'
 						},
+						'version': {
+							'type': 'integer'
+						}
 					},
-					required: ['templateName', 'fields'],
-				},
-				_id: {
-					type: 'string',
-				},
-				type: {
-					type: 'string',
-				},
-				subType: {
-					type: 'string',
-				},
-				action: {
-					type: 'string',
-				},
-				updatedAt: {
-					type: 'string',
-				},
-				createdAt: {
-					type: 'string',
-				},
-				__v: {
-					type: 'integer',
-				},
-			},
-			required: ['data', '_id', 'type', 'subType', 'action', 'updatedAt', 'createdAt', '__v'],
+					'required': [
+						'id',
+						'type',
+						'version'
+					]
+				}
+			]
 		},
-		meta: {
-			type: 'object',
-			properties: {
-				formsVersion: {
-					type: 'array',
-					items: {},
+		'meta': {
+			'type': 'object',
+			'properties': {
+				'formsVersion': {
+					'type': 'array',
+					'items': [
+						{
+							'type': 'object',
+							'properties': {
+								'id': {
+									'type': 'integer'
+								},
+								'type': {
+									'type': 'string'
+								},
+								'version': {
+									'type': 'integer'
+								}
+							},
+							'required': [
+								'id',
+								'type',
+								'version'
+							]
+						}
+					]
 				},
+				'correlation': {
+					'type': 'string'
+				}
 			},
-			required: ['formsVersion'],
-		},
+			'required': [
+				'formsVersion'
+			]
+		}
 	},
-	required: ['responseCode', 'message', 'result', 'meta'],
+	'required': [
+		'responseCode',
+		'message',
+		'result',
+		'meta'
+	]
 }
 const updateSchema = {
 	type: 'object',
