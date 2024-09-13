f565351020a6dcfcb193d919a70c462fd6184ef3
Priyanka <priyanka@tunerlabs.com>
2024-09-13 11:38:31 +0530

    config apis

---
+++ src/integration-tests/config.js/config.spec.js
@@ -0,0 +1,16 @@
+const commonHelper = require('@commonTests')
+const schema = require('./responseSchema')
+jest.setTimeout(10000)
+
+describe('Config APIs ', function () {
+	let userDetails
+	beforeAll(async () => {
+		userDetails = await commonHelper.logIn()
+	})
+
+	it('List Organization and Instance Configurations', async () => {
+		let res = await request.get('/scp/v1/config/list')
+		expect(res.statusCode).toBe(200)
+		expect(res.body).toMatchSchema(schema.listSchema)
+	})
+})
---
+++ src/integration-tests/config.js/responseSchema.js
@@ -0,0 +1,95 @@
+const listSchema = {
+	'type': 'object',
+	'properties': {
+		'responseCode': {
+			'type': 'string'
+		},
+		'message': {
+			'type': 'string'
+		},
+		'result': {
+			'type': 'object',
+			'properties': {
+				'resource': {
+					'type': 'array',
+					'items': [
+						{
+							'type': 'object',
+							'properties': {
+								'review_required': {
+									'type': 'boolean'
+								},
+								'show_reviewer_list': {
+									'type': 'boolean'
+								},
+								'min_approval': {
+									'type': 'integer'
+								},
+								'review_type': {
+									'type': 'string'
+								},
+								'resource_type': {
+									'type': 'string'
+								},
+								'max_task_count': {
+									'type': 'integer'
+								}
+							},
+							'required': [
+								'review_required',
+								'show_reviewer_list',
+								'min_approval',
+								'review_type',
+								'resource_type',
+							]
+						},
+					]
+				},
+				'instance': {
+					'type': 'object',
+					'properties': {
+						'auto_save_interval': {
+							'type': 'integer'
+						},
+						'note_length': {
+							'type': 'integer'
+						}
+					},
+					'required': [
+						'auto_save_interval',
+						'note_length'
+					]
+				}
+			},
+			'required': [
+				'resource',
+				'instance'
+			]
+		},
+		'meta': {
+			'type': 'object',
+			'properties': {
+				'formsVersion': {
+					'type': 'array',
+					'items': {}
+				},
+				'correlation': {
+					'type': 'string'
+				}
+			},
+			'required': [
+				'formsVersion'
+			]
+		}
+	},
+	'required': [
+		'responseCode',
+		'message',
+		'result',
+		'meta'
+	]
+}
+
+module.exports = {
+	listSchema,
+}
