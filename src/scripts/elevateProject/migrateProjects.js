3414b2815845a8cc674fee4fc6438f518ff1dc82
Priyanka <priyanka@tunerlabs.com>
2024-10-15 13:32:47 +0530

    elevate project script changes done

--- src/scripts/elevateProject/migrateProjects.js
+++ src/scripts/elevateProject/migrateProjects.js
@@ -6,8 +6,8 @@
  */
 
 require('module-alias/register')
-require('dotenv').config({ path: '../.env' })
-require('../configs/events')()
+require('dotenv').config({ path: '../../.env' })
+require('../../configs/events')()
 const path = require('path')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter
 const entityTypeService = require('@services/entity-types')
@@ -18,6 +18,8 @@ const resourceQueries = require('@database/queries/resources')
 const _ = require('lodash')
 const MongoClient = require('mongodb').MongoClient
 const { v4: uuidv4 } = require('uuid')
+const userRequest = require('@requests/user')
+var ObjectId = require('mongodb').ObjectID
 
 //get mongo db url
 const mongoUrl = process.env.MONGODB_URL
@@ -81,12 +83,19 @@ const dbName = mongoUrl.split('/').pop()
 			}
 		})
 
+		//get default userId
+		const DEFAULT_USER_ID = await getDefaultUserId()
+		if (!DEFAULT_USER_ID) {
+			throw new Error('Failed to get default org admin')
+		}
+
 		// Get all project templates
 		const projectTemplates = await db
 			.collection('projectTemplates')
 			.find({
 				status: 'published',
 				isReusable: true,
+				// _id: ObjectId('66584ba3fa28840a616722cb')
 			})
 			.project({ _id: 1 })
 			.toArray()
@@ -109,6 +118,10 @@ const dbName = mongoUrl.split('/').pop()
 				.collection('projectTemplates')
 				.find({ _id: { $in: templateIds } })
 				.toArray()
+
+			//Fetch user and org details
+			let userIds = templates.map((template) => template.createdBy)
+			let userOrgMap = await getUserOrgDetails(userIds)
 
 			await Promise.all(
 				templates.map(async (template) => {
@@ -159,7 +172,7 @@ const dbName = mongoUrl.split('/').pop()
 						}
 
 						// Convert template
-						let convertedTemplate = await convertTemplate(template)
+						let convertedTemplate = await convertTemplate(template, userOrgMap, DEFAULT_USER_ID)
 						if (!convertedTemplate.success) {
 							throw new Error(convertedTemplate.error)
 						}
@@ -221,11 +234,38 @@ const dbName = mongoUrl.split('/').pop()
 	}
 })()
 
+//get default org admin
+async function getDefaultUserId() {
+	let defaultUserId = null
+	let orgDetails = await userRequest.fetchOrg(process.env.DEFAULT_ORG_ID)
+	if (
+		orgDetails.success &&
+		Array.isArray(orgDetails?.data?.result?.org_admin) &&
+		orgDetails.data.result.org_admin.length > 0
+	) {
+		defaultUserId = orgDetails.data.result.org_admin[0]
+	}
+	return defaultUserId
+}
+
+//get user org id
+async function getUserOrgDetails(userIds) {
+	let userOrgMap = {}
+	const users = await userRequest.list('all', '', '', '', '', {
+		user_ids: userIds,
+	})
+
+	if (users.success && users.data?.result?.data?.length > 0) {
+		userOrgMap = _.keyBy(users.data.result.data, 'id')
+	}
+
+	return userOrgMap
+}
+
 async function checkProjectExist(templateId) {
 	try {
 		let project = await resourceQueries.findOne(
 			{
-				organization_id: process.env.DEFAULT_ORG_ID,
 				published_id: templateId,
 			},
 			{
@@ -250,8 +290,15 @@ async function checkProjectExist(templateId) {
 	}
 }
 
-async function convertTemplate(template) {
+async function convertTemplate(template, userOrgMap, DEFAULT_USER_ID) {
 	try {
+		let userId = DEFAULT_USER_ID
+		let orgId = process.env.DEFAULT_ORG_ID
+		if (userOrgMap[template.createdBy]) {
+			userId = template.createdBy
+			orgId = userOrgMap[template.createdBy].organization.id
+		}
+
 		// Helper function to convert resources
 		const convertResources = (resources) =>
 			resources.map(({ name, link }) => ({
@@ -299,7 +346,8 @@ async function convertTemplate(template) {
 				? convertResources(template.learningResources)
 				: [],
 			licenses: 'cc_by_4.0',
-			created_by: template.createdBy,
+			created_by: userId.toString(),
+			organization_id: orgId.toString(),
 			published_id: template._id,
 			tasks: template.taskDetails ? template.taskDetails.map(convertTask) : [],
 		}
@@ -450,7 +498,7 @@ async function createProjectAndEntities(templateId, templateData, entitiesToCrea
 			templateId,
 			templateData,
 			templateData.created_by,
-			process.env.DEFAULT_ORG_ID
+			templateData.organization_id
 		)
 
 		if (projectCreationResponse.success) {
--- src/scripts/readme.md
+++ src/scripts/readme.md
@@ -1,16 +1,27 @@
 # Script Excution
 
+### Upload Default Certificate Template to Cloud
+
+This script uploads the default certificate template to the cloud. It should be run only once during the initial project setup.
+
 -   Navigate to the scripts folder using the following command
     ```bash
     cd src/scripts
     ```
 -   Run **uploadCertificateBaseTemplate.js** only once while setup the project
-
     ```bash
-    node -r module-alias/register uploadCertificateBaseTemplate.js
+        node -r module-alias/register uploadCertificateBaseTemplate.js
     ```
 
--   Run **migrateProjects.js** only once while setup the project
+### Migration Script for copy all elevate project templates to SCP
+
+This script migrates all existing project templates of elevate project
+
+-   Navigate to the scripts folder using the following command
     ```bash
-    node migrateProjects.js
+        cd src/scripts/elevateProject
+    ```
+-   Run the script
+    ```bash
+        node migrateProjects.js
     ```
