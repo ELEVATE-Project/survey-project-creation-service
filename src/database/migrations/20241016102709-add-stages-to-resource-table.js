6a6b661ee2a1793ba55c55ea2db50e90b7dd1d1a
Priyanka <priyankapratheepan@gmail.com>
2024-10-18 14:40:45 +0530

    added index for stage

--- src/database/migrations/20241016102709-add-stages-to-resource-table.js
+++ src/database/migrations/20241016102709-add-stages-to-resource-table.js
@@ -8,9 +8,17 @@ module.exports = {
 			type: Sequelize.ENUM('CREATION', 'REVIEW', 'COMPLETION'),
 			defaultValue: 'CREATION',
 		})
+
+		// Add index to the 'stage' column for performance improvement
+		await queryInterface.addIndex('resources', ['stage'], {
+			name: 'resources_stage_index',
+		})
 	},
 
 	async down(queryInterface, Sequelize) {
+		// Remove the index on 'stage' column
+		await queryInterface.removeIndex('resources', 'resources_stage_index')
+
 		await queryInterface.removeColumn('resources', 'stage')
 	},
 }
