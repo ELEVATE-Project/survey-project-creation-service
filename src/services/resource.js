8a9cdb945a6fbda50af1d80601ea70a417306063
adithya_dinesh <adithya@shikshalokam.org>
2024-10-28 13:58:01 +0530

    count and listing issue fix in up for review list

--- src/services/resource.js
+++ src/services/resource.js
@@ -490,12 +490,17 @@ module.exports = class resourceHelper {
 					['resource_id']
 				)
 
-				result.in_progress_count = distinctResourceIds.count
 				inProgressResources = utils.getUniqueElements(distinctResourceIds.resource_ids)
-			}
-			if (common.STATUS in queryParams && queryParams[common.STATUS] === common.REVIEW_STATUS_INPROGRESS) {
+				const in_progress_count = await resourceQueries.count({
+					id: {
+						[Op.in]: inProgressResources,
+					},
+					status: { [Op.in]: [common.REVIEW_STATUS_INPROGRESS] },
+				})
+				result.in_progress_count = in_progress_count
 				finalResourceIds = inProgressResources
-			} else {
+			}
+			if (!(common.STATUS in queryParams && queryParams[common.STATUS] === common.REVIEW_STATUS_INPROGRESS)) {
 				// fetch the resources types of an organization based on parallel and sequential review type
 				let { sequential: resourceTypesInSequentialReview, parallel: resourceTypesInParallelReview } =
 					await this.fetchResourceReviewTypes(organization_id)
@@ -582,14 +587,18 @@ module.exports = class resourceHelper {
 				user_id: {
 					[Op.notIn]: [user_id],
 				},
-				status: {
+			}
+			if (common.STATUS in queryParams && queryParams[common.STATUS] === common.REVIEW_STATUS_INPROGRESS) {
+				resourceFilter.status = common.REVIEW_STATUS_INPROGRESS
+			} else {
+				resourceFilter.status = {
 					[Op.notIn]: [
 						common.RESOURCE_STATUS_PUBLISHED,
 						common.RESOURCE_STATUS_REJECTED,
 						common.RESOURCE_STATUS_REJECTED_AND_REPORTED,
 						common.RESOURCE_STATUS_DRAFT,
 					],
-				},
+				}
 			}
 			if (searchText != '')
 				resourceFilter.title = {
