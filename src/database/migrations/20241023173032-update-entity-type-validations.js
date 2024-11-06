95b77c34594c1de7d6c379ecd570d92488846464
Priyanka <priyankapratheepan@gmail.com>
2024-11-06 10:22:47 +0530

    length validation

--- src/database/migrations/20241023173032-update-entity-type-validations.js
+++ src/database/migrations/20241023173032-update-entity-type-validations.js
@@ -173,7 +173,7 @@ function transformValidation(validation, entityType) {
 		}
 		transformedData.push(data)
 	}
-	if (['keywords', 'title', 'solution_details'].includes(entityType)) {
+	if (['keywords', 'title', 'solution_details', 'objective', 'name'].includes(entityType)) {
 		let data = { type: 'max_length', value: 256 }
 		if (getNewMessage(entityType, 'max_length')) {
 			data.message = getNewMessage(entityType, 'max_length')
