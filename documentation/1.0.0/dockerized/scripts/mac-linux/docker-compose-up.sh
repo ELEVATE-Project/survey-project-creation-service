3d2854a3b63b7a4e2fa853dfc1a9d51a41ddce54
Priyanka <priyankapratheepan@gmail.com>
2024-11-21 16:45:32 +0530

    remove log

--- documentation/1.0.0/dockerized/scripts/mac-linux/docker-compose-up.sh
+++ documentation/1.0.0/dockerized/scripts/mac-linux/docker-compose-up.sh
@@ -3,8 +3,6 @@
 # Get the directory of the shell script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-log "$SCRIPT_DIR script directory"
-
 # Set environment variables
 export notification_env="$SCRIPT_DIR/notification_env"
 export scheduler_env="$SCRIPT_DIR/scheduler_env"
