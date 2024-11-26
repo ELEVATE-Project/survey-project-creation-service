c70679c617daf3d2b2e89eea9a62eff0bea5c12d
Priyanka <priyankapratheepan@gmail.com>
2024-11-26 17:03:32 +0530

    initial commit

---
+++ src/controllers/v1/rollouts.js
@@ -0,0 +1,46 @@
+/**
+ * name : rollouts.js
+ * author : Priyanka Pradeep
+ * created-date : 26-Nov-2024
+ * Description : Controller for Rollouts
+ */
+
+const rolloutService = require('@services/rollouts')
+const common = require('@constants/common')
+module.exports = class rollouts {
+	/**
+	 * Create or Update Rollout.
+	 * @method
+	 * @name update
+	 * @param {Integer} id  rollout id.
+	 * @param {Object} body  rollout data
+	 * @returns {JSON} - Detail of rollout as response.
+	 */
+	async update(req) {
+		try {
+			if (req.params.id) {
+				let rollout = {}
+				if (req.method === common.REQUEST_METHOD_DELETE) {
+					rollout = await rolloutService.delete(req.params.id, req.decodedToken.id)
+				} else {
+					rollout = await rolloutService.update(
+						req.params.id,
+						req.body,
+						req.decodedToken.id,
+						req.decodedToken.organization_id
+					)
+				}
+				return rollout
+			} else {
+				const rollout = await rolloutService.create(
+					req.body,
+					req.decodedToken.id,
+					req.decodedToken.organization_id
+				)
+				return rollout
+			}
+		} catch (error) {
+			return error
+		}
+	}
+}
---
+++ src/services/rollouts.js
@@ -0,0 +1,136 @@
+/* eslint-disable no-useless-catch */
+/* eslint-disable no-undef */
+const httpStatusCode = require('@generics/http-status')
+const responses = require('@helpers/responses')
+const common = require('@constants/common')
+const userRequests = require('@requests/user')
+const utils = require('@generics/utils')
+
+module.exports = class RolloutsHelper {
+	/**
+	 * Rollout create
+	 * @method
+	 * @name create
+	 * @param {Object} req - request data.
+	 * @returns {JSON} - project id
+	 */
+	static async create(bodyData, loggedInUserId, orgId) {
+		try {
+			//validate the title length
+			const isTitleInvalid = utils.validateTitle(bodyData.title)
+			if (isTitleInvalid) {
+				return responses.failureResponse({
+					message: 'CHARACTER_LIMIT_EXCEED',
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
+
+			//validation for start date and end date
+			if (bodyData.start_date && bodyData.end_date) {
+				const startDate = new Date(bodyData.start_date)
+				const endDate = new Date(bodyData.end_date)
+
+				if (startDate > endDate) {
+					return responses.failureResponse({
+						message: 'INVALID_DATE_RANGE',
+						statusCode: httpStatusCode.bad_request,
+						responseCode: 'CLIENT_ERROR',
+					})
+				}
+			}
+
+			//validate the resource
+			let resource = await resourceQueries.findOne({
+				id: bodyData.resource_id,
+				organization_id: orgId,
+				status: common.RESOURCE_STATUS_PUBLISHED,
+				stage: common.RESOURCE_STAGE_COMPLETION,
+			})
+
+			if (resource?.id) {
+				return responses.failureResponse({
+					message: 'RESOURCE_NOT_FOUND',
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
+
+			let rolloutData = {
+				title: bodyData.title,
+				resource_type: resource.type,
+				resource_id: resource.id,
+				status: common.ROLLOUT_STATUS_PENDING,
+				start_date: bodyData.start_date,
+				end_date: bodyData.end_date,
+				type: common.ROLLOUT_TYPE_PROGRAM,
+				user_id: loggedInUserId,
+				organization_id: orgId,
+				created_by: loggedInUserId,
+				updated_by: loggedInUserId,
+			}
+
+			let rolloutCreate
+			try {
+				//create rollout
+				rolloutCreate = await rolloutQueries.create(rolloutData)
+
+				// upload to blob
+				const rolloutId = rolloutCreate.id
+				const fileName = `${loggedInUserId}${rolloutId}rollout.json`
+
+				const rolloutUploadStatus = await rolloutService.uploadToCloud(
+					fileName,
+					rolloutCreate.id,
+					common.PROJECT,
+					loggedInUserId,
+					bodyData
+				)
+
+				if (
+					rolloutUploadStatus.result.status == httpStatusCode.ok ||
+					rolloutUploadStatus.result.status == httpStatusCode.created
+				) {
+					let filter = {
+						id: resourceId,
+						organization_id: orgId,
+					}
+
+					let updateData = {
+						updated_by: loggedInUserId,
+						blob_path: rolloutUploadStatus.blob_path,
+					}
+
+					const [updateCount] = await rolloutQueries.updateOne(filter, updateData, {
+						returning: true,
+						raw: true,
+					})
+
+					if (updateCount === 0) {
+						return responses.failureResponse({
+							message: 'ROLLOUT_NOT_FOUND',
+							statusCode: httpStatusCode.bad_request,
+							responseCode: 'CLIENT_ERROR',
+						})
+					}
+				} else {
+					throw new Error('FILE_UPLOADED_FAILED')
+				}
+			} catch (error) {
+				return responses.failureResponse({
+					message: error.message || error,
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
+
+			return responses.successResponse({
+				statusCode: httpStatusCode.ok,
+				message: 'ROLLOUT_CREATED_SUCCESSFULLY',
+				result: { id: rolloutCreate.id },
+			})
+		} catch (error) {
+			throw error
+		}
+	}
+}
