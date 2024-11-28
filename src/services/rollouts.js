07f9c3c50113b3e5ba121aeef9d87b258bdfc703
Priyanka <priyankapratheepan@gmail.com>
2024-11-28 17:11:07 +0530

    Rollout update api

--- src/services/rollouts.js
+++ src/services/rollouts.js
@@ -13,7 +13,7 @@ module.exports = class RolloutsHelper {
 	 * @method
 	 * @name create
 	 * @param {Object} req - request data.
-	 * @returns {JSON} - project id
+	 * @returns {JSON} - rollout id
 	 */
 	static async create(bodyData, loggedInUserId, orgId) {
 		const transaction = await db.sequelize.transaction()
@@ -120,6 +120,107 @@ module.exports = class RolloutsHelper {
 		} catch (error) {
 			await transaction.rollback() // Rollback transaction on any error
 			throw error
+		}
+	}
+
+	/**
+	 * Rollout update
+	 * @method
+	 * @name update
+	 * @param {Integer} rolloutId - Rollout Id.
+	 * @param {Object} bodyData - request data.
+	 * @param {String} loggedInUserId - userId
+	 * @param {String} orgId - organization id
+	 * @returns {JSON} - rollout update response.
+	 */
+
+	static async update(rolloutId, bodyData, loggedInUserId, orgId) {
+		try {
+			let rollout = await rolloutQueries.findOne({
+				id: rolloutId,
+				organization_id: orgId,
+			})
+
+			if (!rollout?.id) {
+				return responses.failureResponse({
+					message: 'ROLLOUT_NOT_FOUND',
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
+			//check the rollout
+			if (bodyData.resource_id && bodyData.resource_id != rollout.resource_id) {
+				if (rollout.rollout_date || bodyData.published_id) {
+					return responses.failureResponse({
+						message: 'CANT_CHANGE_RESOURCE',
+						statusCode: httpStatusCode.bad_request,
+						responseCode: 'CLIENT_ERROR',
+					})
+				}
+				let resource = await resourceQueries.findOne({
+					id: bodyData.resource_id,
+					organization_id: orgId,
+					stage: common.RESOURCE_STAGE_COMPLETION,
+				})
+
+				if (!resource?.id) {
+					return responses.failureResponse({
+						message: 'RESOURCE_NOT_FOUND',
+						statusCode: httpStatusCode.bad_request,
+						responseCode: 'CLIENT_ERROR',
+					})
+				}
+			}
+
+			if (bodyData.targeting_criteria) {
+				const fileName = `${loggedInUserId}${rolloutId}rollout.json`
+				const rolloutUploadStatus = await resourceService.uploadToCloud(
+					fileName,
+					rolloutId,
+					common.ROLL_OUT,
+					loggedInUserId,
+					bodyData
+				)
+				if (
+					rolloutUploadStatus.result.status == httpStatusCode.ok ||
+					rolloutUploadStatus.result.status == httpStatusCode.created
+				) {
+					bodyData.blob_path = rolloutUploadStatus.blob_path
+				} else {
+					throw new Error('FILE_UPLOADED_FAILED')
+				}
+			}
+
+			bodyData = _.omit(bodyData, ['id', 'resource_type', 'type', 'organization_id', 'user_id'])
+
+			let filter = {
+				id: rolloutId,
+				organization_id: orgId,
+			}
+
+			let updateData = {
+				updated_by: loggedInUserId,
+				...bodyData,
+			}
+
+			const [updateCount] = await rolloutQueries.updateOne(filter, updateData, {
+				returning: true,
+				raw: true,
+			})
+
+			if (updateCount === 0) {
+				return responses.failureResponse({
+					message: 'ROLLOUT_NOT_FOUND',
+					statusCode: httpStatusCode.bad_request,
+					responseCode: 'CLIENT_ERROR',
+				})
+			}
+		} catch (error) {
+			return responses.failureResponse({
+				message: error.message || error,
+				statusCode: httpStatusCode.bad_request,
+				responseCode: 'CLIENT_ERROR',
+			})
 		}
 	}
 }
