df923a59d47dc5a32234a245e83378499d4abdf9
adithya_dinesh <adithya@shikshalokam.org>
2024-12-02 12:15:30 +0530

    integration test added - issue fix

--- src/integration-tests/rollouts/rollouts.spec.js
+++ src/integration-tests/rollouts/rollouts.spec.js
@@ -17,7 +17,7 @@ describe('Rollout APIs', function () {
 	})
 
 	it('Get list of data managers list', async () => {
-		let res = await request.get('scp/v1/rollouts/getDataManagers').query({ page: 1, limit: 10 })
+		let res = await request.get('/scp/v1/rollouts/getDataManagers').query({ page: 1, limit: 10 })
 		expect(res.statusCode).toBe(200)
 		expect(res.body).toMatchSchema(schema.listSchema)
 	})
