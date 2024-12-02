061dbe5b3c87fa487f7c0951524c1f3278dee760
adithya_dinesh <adithya@shikshalokam.org>
2024-12-02 13:04:29 +0530

    integration test added - issue fix

--- src/integration-tests/rollouts/responseSchema.js
+++ src/integration-tests/rollouts/responseSchema.js
@@ -3349,7 +3349,45 @@ const getDataManagersSchema = {
 	},
 	required: ['responseCode', 'message', 'result', 'meta'],
 }
-
+const getDataManagersEmptyResponseSchema = {
+	type: 'object',
+	properties: {
+		responseCode: {
+			type: 'string',
+		},
+		message: {
+			type: 'string',
+		},
+		result: {
+			type: 'object',
+			properties: {
+				data: {
+					type: 'array',
+					items: {},
+				},
+				count: {
+					type: 'integer',
+				},
+			},
+			required: ['data', 'count'],
+		},
+		meta: {
+			type: 'object',
+			properties: {
+				formsVersion: {
+					type: 'array',
+					items: {},
+				},
+				correlation: {
+					type: 'string',
+				},
+			},
+			required: ['formsVersion', 'correlation'],
+		},
+	},
+	required: ['responseCode', 'message', 'result', 'meta'],
+}
 module.exports = {
 	getDataManagersSchema,
+	getDataManagersEmptyResponseSchema,
 }
