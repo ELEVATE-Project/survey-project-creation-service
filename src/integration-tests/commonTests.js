2c186d6c4f66dd425bc06677a254737b52c8bebc
adithya_dinesh <adithya@shikshalokam.org>
2024-12-02 18:49:38 +0530

    review comments

--- src/integration-tests/commonTests.js
+++ src/integration-tests/commonTests.js
@@ -58,13 +58,18 @@ const verifyUserRole = async () => {
 			}
 
 			// Run role checks concurrently for content_creator and reviewer roles
-			const [existingCreatorRole, existingReviewerRole] = await Promise.all([
+			const [existingCreatorRole, existingReviewerRole, existingRolloutManagerRole] = await Promise.all([
 				request.get('/user/v1/user-role/list').set(defaultHeaders).query({
 					title: 'content_creator',
 					organization_id: 1,
 				}),
 				request.get('/user/v1/user-role/list').set(defaultHeaders).query({
 					title: 'reviewer',
+					organization_id: 1,
+				}),
+
+				request.get('/user/v1/user-role/list').set(defaultHeaders).query({
+					title: 'rollout_manager',
 					organization_id: 1,
 				}),
 			])
@@ -97,7 +102,10 @@ const verifyUserRole = async () => {
 			}
 
 			// Add rollout_manager role creation promise
-			if (existingReviewerRole.statusCode === 400 || !existingReviewerRole.body.result?.data?.length) {
+			if (
+				existingRolloutManagerRole.statusCode === 400 ||
+				!existingRolloutManagerRole.body.result?.data?.length
+			) {
 				const createRolloutManagerRole = request.post('/user/v1/user-role/create').set(defaultHeaders).send({
 					title: 'rollout_manager',
 					user_type: 0,
