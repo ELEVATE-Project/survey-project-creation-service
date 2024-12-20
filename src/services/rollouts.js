bb6f81145f7953e8296677cc11075fc010afcb2a
priyanka-TL <priyankapratheepan@gmail.com>
2024-12-20 19:25:31 +0530

    fix for date

--- src/services/rollouts.js
+++ src/services/rollouts.js
@@ -42,14 +42,15 @@ module.exports = class RolloutsHelper {
 				resource_type: resource.type,
 				resource_id: resource.id,
 				status: common.ROLLOUT_STATUS_PENDING,
-				start_date: bodyData.start_date,
-				end_date: bodyData.end_date,
 				type: common.ROLLOUT_TYPE_PROGRAM,
 				user_id: loggedInUserId,
 				organization_id: orgId,
 				created_by: loggedInUserId,
 				updated_by: loggedInUserId,
 			}
+
+			if (bodyData.start_date) rolloutData.start_date = bodyData.start_date
+			if (bodyData.end_date) rolloutData.end_date = bodyData.end_date
 
 			let rolloutCreate
 			try {
