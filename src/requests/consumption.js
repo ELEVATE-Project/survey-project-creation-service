7ef745029aa09f16c42f83d9e8780405c5ed8d40
adithya_dinesh <adithya@shikshalokam.org>
2025-01-15 18:50:54 +0530

    certificate issue fix

--- src/requests/consumption.js
+++ src/requests/consumption.js
@@ -859,7 +859,7 @@ const processTargetingCriteria = async (targetingData) => {
 	})
 
 	// convert the 'entityType' array to coma separated string
-	scope.entityType = scope?.entityType ? scope?.entityType.join(',') : ''
+	scope.entityType = scope?.entityType ? [...new Set(scope.entityType)].join(',') : ''
 	// refactor metaInformation to remove duplicates
 	Object.keys(metaInformation).forEach((key) => {
 		if (Array.isArray(metaInformation[key] && metaInformation[key].length > 0)) {
