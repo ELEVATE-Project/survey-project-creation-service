4cc9393590b21997a37b14d4ce541a8fa72b9575
priyanka-TL <priyankapratheepan@gmail.com>
2025-01-29 16:12:01 +0530

    Integration added

---
+++ src/integration-tests/programs/responseSchema.js
@@ -0,0 +1,166 @@
+const createSchema = {
+	type: 'object',
+	properties: {
+		responseCode: {
+			type: 'string',
+		},
+		message: {
+			type: 'string',
+		},
+		result: {
+			type: 'object',
+			properties: {
+				id: {
+					type: 'integer',
+				},
+			},
+			required: ['id'],
+		},
+		meta: {
+			type: 'object',
+			properties: {
+				formsVersion: {
+					type: 'array',
+					items: {},
+				},
+				correlation: {
+					type: 'string',
+				},
+			},
+		},
+	},
+	required: ['responseCode', 'message', 'result'],
+}
+
+const detailSchema = {
+	type: 'object',
+	properties: {
+		responseCode: {
+			type: 'string',
+		},
+		message: {
+			type: 'string',
+		},
+		result: {
+			type: 'object',
+			properties: {
+				organization: {
+					type: 'object',
+					properties: {
+						id: {
+							type: 'integer',
+						},
+						name: {
+							type: 'string',
+						},
+						code: {
+							type: 'string',
+						},
+					},
+					required: ['id', 'name', 'code'],
+				},
+				title: {
+					type: 'string',
+				},
+				objective: {
+					type: 'string',
+				},
+				licenses: {
+					type: 'array',
+					items: [
+						{
+							type: 'object',
+							properties: {
+								label: {
+									type: 'string',
+								},
+								value: {
+									type: 'string',
+								},
+							},
+							required: ['label', 'value'],
+						},
+					],
+				},
+				id: {
+					type: 'integer',
+				},
+				type: {
+					type: 'string',
+				},
+				status: {
+					type: 'string',
+				},
+				user_id: {
+					type: 'string',
+				},
+				organization_id: {
+					type: 'string',
+				},
+				created_by: {
+					type: 'string',
+				},
+				updated_by: {
+					type: 'string',
+				},
+				submitted_on: {
+					type: 'null',
+				},
+				published_on: {
+					type: 'null',
+				},
+				last_reviewed_on: {
+					type: 'null',
+				},
+				is_under_edit: {
+					type: 'boolean',
+				},
+				created_at: {
+					type: 'string',
+				},
+				updated_at: {
+					type: 'string',
+				},
+				deleted_at: {
+					type: 'null',
+				},
+			},
+			required: [
+				'organization',
+				'title',
+				'id',
+				'type',
+				'status',
+				'user_id',
+				'organization_id',
+				'created_by',
+				'updated_by',
+				'submitted_on',
+				'published_on',
+				'last_reviewed_on',
+				'is_under_edit',
+				'created_at',
+				'updated_at',
+				'deleted_at',
+			],
+		},
+		meta: {
+			type: 'object',
+			properties: {
+				formsVersion: {
+					type: 'array',
+					items: {},
+				},
+				correlation: {
+					type: 'string',
+				},
+			},
+		},
+	},
+	required: ['responseCode', 'message', 'result'],
+}
+
+module.exports = {
+	createSchema,
+	detailSchema,
+}
