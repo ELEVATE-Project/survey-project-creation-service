2416ed2b2b504bb14cd49794d2929ee493f04370
priyanka-TL <priyankapratheepan@gmail.com>
2025-02-18 13:19:09 +0530

    added comments

--- src/services/rollouts.js
+++ src/services/rollouts.js
@@ -293,12 +293,13 @@ module.exports = class RolloutsHelper {
 			const programIds = programRollouts.map((rollout) => rollout.resource_id)
 
 			//filter based on multiple resource_types
+			//Remove resource type program hence its a listing for single resource rollout
 			let resourceTypes =
 				queryParams.resource_type && queryParams.resource_type !== ''
 					? queryParams.resource_type.split(',').filter((type) => type !== common.RESOURCE_TYPE_PROGRAM)
 					: ''
 
-			//Remove resource type program hence its a listing for single resource rollout
+			//resource_type are not passing then return all rollout expect resource_type program
 			filters.resource_type = resourceTypes.length
 				? { [Op.in]: resourceTypes }
 				: { [Op.not]: common.RESOURCE_TYPE_PROGRAM }
