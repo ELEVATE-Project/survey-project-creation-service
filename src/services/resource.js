7cf8df7a82b332df91ac7474d3b87867bb244f8d
adithya_dinesh <adithya@shikshalokam.org>
2025-11-10 12:56:23 +0530

    @coderabbitai

--- src/services/resource.js
+++ src/services/resource.js
@@ -520,7 +520,7 @@ module.exports = class resourceHelper {
 	 * @param {Object} queryParams -  queryParams contain sort details like sort_by, sort_order
 	 * @returns {JSON} - Response contain sort filter
 	 */
-	static async constructSortOptions(sort_by, sort_order, defaultSortBy = common.CREATED_AT) {
+	static async constructSortOptions(sort_by, sort_order, defaultSortBy = common.UPDATED_AT) {
 		let sort = {}
 		if (sort_by && sort_order) {
 			sort.sort_by = sort_by
