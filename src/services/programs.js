fb9632413889a85213cc0a0adb8564f37544eb16
adithya_dinesh <adithya@shikshalokam.org>
2025-12-09 17:43:07 +0530

    status update

--- src/services/programs.js
+++ src/services/programs.js
@@ -1518,7 +1518,7 @@ async function createProgramChild(parentId, programData, loggedInUserId) {
 			created_by: loggedInUserId,
 			organization_code,
 			tenant_code,
-			status: { [Op.notIn]: [common.PUBLISHED_STATUS] },
+			status: { [Op.notIn]: [common.RESOURCE_STATUS_PUBLISHED] },
 		})
 		if (!findExistingOpenChild?.id) {
 			const currentLatestVersion = await resourceService.findLatestVersionOfResource(
