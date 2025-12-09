7a4bd7d8e80afc694d0c13a1d80c7fcf9c899f60
adithya_dinesh <adithya@shikshalokam.org>
2025-12-09 15:39:28 +0530

    small fix

--- src/services/rollouts.js
+++ src/services/rollouts.js
@@ -61,7 +61,7 @@ module.exports = class RolloutsHelper {
 
 			if (bodyData.start_date) rolloutData.start_date = bodyData.start_date
 			if (bodyData.end_date) rolloutData.end_date = bodyData.end_date
-			if (isSolutionType === true) rolloutData.parent_id = bodyData.parent_id
+			if (isSolutionType === true) rolloutData.parent_id = bodyData?.parent_id || 0
 
 			let rolloutCreate
 			try {
